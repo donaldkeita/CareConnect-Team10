f11be37ed50297f96904dcb3a2fa0f075a4097db
Sahil Kapoor <sahilkap>
2026-09-08 16:30:20 -0400

    Add medication entry screen and update medications flow

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'screens/home_screen.dart';
 import 'screens/appointments_screen.dart';
 import 'screens/medications_screen.dart';
+import 'screens/add_medication_screen.dart';
 
 void main() {
   runApp(const CareConnectApp());
@@ -13,20 +15,20 @@ class CareConnectApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'CareConnect',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFF8F9FB),
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF24466F),
+          seedColor: const Color(0xFF173B68),
         ),
+        useMaterial3: true,
       ),
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen(),
         '/appointments': (context) => AppointmentsScreen(),
-        '/medications': (context) => MedicationsScreen(),
+        '/medications': (context) => const MedicationsScreen(),
+        '/add-medication': (context) => const AddMedicationScreen(),
       },
     );
   }
